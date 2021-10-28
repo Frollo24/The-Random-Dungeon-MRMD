36ffc6a03de9c6c734781fa072b8abce1214f431
Ponderato <68611385+Ponderato@users.noreply.github.com>
2021-10-28 21:21:41 +0200

    Third Person Movement

--- TRD-MRMD/Assets/Scripts/CharacterMovement.cs
+++ TRD-MRMD/Assets/Scripts/CharacterMovement.cs
@@ -18,15 +18,19 @@ public class CharacterMovement : MonoBehaviour
     {
         if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
 
             Vector3 movementDir = new Vector3(horizontalInput, 0, verticalInput).normalized;
 
-            transform.position += movementDir * speed * Time.deltaTime;
+            //Make sure we are moving
+            if (movementDir.magnitude >= 0.1f)
+            {
+                transform.position += movementDir * speed * Time.deltaTime;
 
-            Quaternion rotateTo = Quaternion.LookRotation(movementDir);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, rotationSpeed * Time.deltaTime);
+                Quaternion rotateTo = Quaternion.LookRotation(movementDir);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, rotationSpeed * Time.deltaTime);
+            }  
         }
 
         
