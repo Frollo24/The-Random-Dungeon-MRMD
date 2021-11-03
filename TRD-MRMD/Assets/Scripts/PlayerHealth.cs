2c393351fd21ed08b1b45559861b7cd1ed558d1c
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2021-11-03 18:30:33 +0100

    Added basic life system

---
+++ TRD-MRMD/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int currentHealth;
+
+    // Debug stuff
+    [Header("Debugging options")]
+    [SerializeField] private KeyCode debugKeyDamage = KeyCode.Space;
+    [SerializeField] private int debugDamageAmt = 10;
+    [SerializeField] private KeyCode debugKeyHealth = KeyCode.Return;
+    [SerializeField] private int debugHealthAmt = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(debugKeyDamage)) //TODO use a keycode which doesn't make conflict
+        {
+            TakeDamage(debugDamageAmt);
+        }
+        if (Input.GetKeyDown(debugKeyHealth)) //TODO use a keycode which doesn't make conflict
+        {
+            RestoreHealth(debugHealthAmt);
+        }
+#endif
+        if (currentHealth <= 0)
+        {
+            TakeDeath();
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    void RestoreHealth(int health)
+    {
+        currentHealth += health;
+    }
+
+    void TakeDeath()
+    {
+        //TODO make proper death
+        Debug.Log("Death");
+        currentHealth = 100;
+    }
+}
