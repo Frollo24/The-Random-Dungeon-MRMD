8a93021003e6f7ef3841ba12b8a8cf6ec9af3adf
Ponderato <68611385+Ponderato@users.noreply.github.com>
2021-11-25 16:43:39 +0100

    Basic Magic System implemented

---
+++ TRD-MRMD/Assets/Scripts/PlayerMagic.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMagic : MonoBehaviour
+{
+    public int maxMagic = 100;
+    public int currentMagic;
+
+    public MagicBarBehaviour magicBar;
+
+    //Debug stuff
+    [Header("Debugging options")]
+    [SerializeField] private KeyCode debugKeySpellCost = KeyCode.M;
+    [SerializeField] private int debugSpellCostAmt = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMagic = maxMagic;
+        magicBar.setMaxMagic(maxMagic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(debugKeySpellCost))
+        {
+            CastSpell(debugSpellCostAmt);
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            RestoreMagic(10);
+        }
+#endif
+    }
+
+    public
+        void CastSpell(int spellCost)
+    {
+        if(currentMagic >= spellCost)
+        {
+            currentMagic -= spellCost;
+            magicBar.setMagic(currentMagic);
+        }
+    }
+
+    public void RestoreMagic(int magicAmt)
+    {
+        currentMagic += magicAmt;
+        if(currentMagic > maxMagic)
+        {
+            currentMagic = maxMagic;
+        }
+        magicBar.setMagic(currentMagic);
+    }
+}
